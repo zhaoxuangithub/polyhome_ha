70b15f1d1477f66a3c619163cc6d7d7c0d006e6b
zhangxuesong <453025053@qq.com>
2018-03-15 16:40:06 +0800

    A 红外人体探测器用老固件

--- custom_components/binary_sensor/polypirsensor.py
+++ custom_components/binary_sensor/polypirsensor.py
@@ -53,7 +53,7 @@ def setup_platform(hass, config, add_devices, discovery_info=None):
             if pack_list[0] == '0xa0' and pack_list[9] == '0x1':
                 # 0xa0 0xc6 0x11 0x5 0x5 0x40 0x11 0x5 0x79 0x1 0x5b
                 dev.set_available(True)
-                dev.set_state(False)
+                dev.set_state(True)
             if pack_list[0] == '0xa0' and pack_list[8] == '0xcc':
                 dev.set_available(True)
                 dev.heart_beat()
