26a227649964bdd027cff1e3f7f6dd1c13cdcf25
zhangxuesong <453025053@qq.com>
2018-03-28 21:02:58 +0800

    F 修复摄像头一直调用 api/proxy问题

--- custom_components/camera/lecamera.py
+++ custom_components/camera/lecamera.py
@@ -1,14 +1,16 @@
 import os
+import json
 import logging
-import homeassistant.util.dt as dt_util
-from homeassistant.components.camera import Camera
+import requests
 import voluptuous as vol
+
+import homeassistant.util.dt as dt_util
+import homeassistant.helpers.config_validation as cv
+from homeassistant.components.camera import Camera
 from homeassistant.components.sensor import PLATFORM_SCHEMA
 from homeassistant.const import (CONF_NAME)
-import homeassistant.helpers.config_validation as cv
-import json
 from requests.exceptions import ConnectionError as ConnectError, HTTPError, Timeout
-import requests
+
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -20,7 +22,7 @@ LEAPPSECRET = '337cb9f908644ca78c09dc1f0d5292'
 SERVICE_CAPTURE = 'camera_capture'
 
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
-    vol.Optional(CONF_NAME): cv.string,
+    vol.Optional(CONF_NAME): cv.string
 })
 
 
@@ -107,7 +109,7 @@ class LeCamera(Camera):
     @property
     def should_poll(self):
         """Camera should poll periodically."""
-        return True
+        return False
 
     @property
     def motion_detection_enabled(self):
@@ -116,20 +118,21 @@ class LeCamera(Camera):
 
     @property
     def device_state_attributes(self):
-        return {'platform': 'polylecheng'}
+        return {'platform': 'lecamera'}
+
+    @property
+    def entity_picture(self):
+        """Return a link to the camera feed as entity picture."""
+        return None
 
     def camera_image(self):
         """Return a faked still image response."""
-        # image_path = os.path.join(
-        #     os.path.dirname(__file__), 'lecamera.jpg')
-        # with open(image_path, 'rb') as file:
-        #     return file.read()
         return None
 
-    def enable_motion_detection(self):
-        """Enable the Motion detection in base station (Arm)."""
-        self._motion_status = True
+    # def enable_motion_detection(self):
+    #     """Enable the Motion detection in base station (Arm)."""
+    #     self._motion_status = True
 
-    def disable_motion_detection(self):
-        """Disable the motion detection in base station (Disarm)."""
-        self._motion_status = False
+    # def disable_motion_detection(self):
+    #     """Disable the motion detection in base station (Disarm)."""
+    #     self._motion_status = False
